83c22a6cb34ea35db75cd0b689980368999f2c88
napdivad <david.l.pan@gmail.com>
2016-02-05 20:28:19 -0800

    asdf

--- sobel_calc.cpp
+++ sobel_calc.cpp
@@ -15,8 +15,9 @@ void grayScale(Mat& __restrict img, Mat& __restrict img_gray_out)
 	const int COLS = IMG_WIDTH;
 	
   //for (int i=0; i< (ROWS & ~3); i++) {
+	int asdf[100]
     for (int j=0; j < 64; j++) {
-      img_gray_out.data[j] = j;
+    	asdf[j] = j;
     }
 		//}
 	/*
