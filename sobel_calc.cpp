bb41b35942395521c06b98f44e6a32d9591d30c8
napdivad <david.l.pan@gmail.com>
2016-02-05 23:31:49 -0800

    same loop

--- sobel_calc.cpp
+++ sobel_calc.cpp
@@ -89,6 +89,19 @@ void sobelCalc(Mat& img_gray, Mat& img_sobel_out, int side)
 
   // Calc the y convolution
   for (int i=1; i<ROWS-1; i++) {
+		
+    for (int j=col_begin; j<col_end; j++) {
+      sobel = abs(img_gray.data[IMG_WIDTH*(i-1) + (j-1)] -
+			  img_gray.data[IMG_WIDTH*(i+1) + (j-1)] +
+			  2*img_gray.data[IMG_WIDTH*(i-1) + (j)] -
+			  2*img_gray.data[IMG_WIDTH*(i+1) + (j)] +
+			  img_gray.data[IMG_WIDTH*(i-1) + (j+1)] -
+			  img_gray.data[IMG_WIDTH*(i+1) + (j+1)]);
+
+      sobel = (sobel > 255) ? 255 : sobel;
+      asdfx[IMG_WIDTH*(i) + j] = sobel;
+    }
+		
     for (int j=col_begin; j<col_end; j++) {
      sobel = abs(img_gray.data[IMG_WIDTH*(i-1) + (j-1)] -
 		   img_gray.data[IMG_WIDTH*(i-1) + (j+1)] +
@@ -99,11 +112,12 @@ void sobelCalc(Mat& img_gray, Mat& img_sobel_out, int side)
 
      sobel = (sobel > 255) ? 255 : sobel;
 
-     asdfx[IMG_WIDTH*(i) + j] = sobel;
+     asdfy[IMG_WIDTH*(i) + j] = sobel;
     }
   }
 	
   // Calculate the x convolution
+	/*
   for (int i=1; i<ROWS-1; i++) {
     for (int j=col_begin; j<col_end; j++) {
       sobel = abs(img_gray.data[IMG_WIDTH*(i-1) + (j-1)] -
@@ -114,9 +128,10 @@ void sobelCalc(Mat& img_gray, Mat& img_sobel_out, int side)
 			  img_gray.data[IMG_WIDTH*(i+1) + (j+1)]);
 
       sobel = (sobel > 255) ? 255 : sobel;
-      asdfy[IMG_WIDTH*(i) + j] = sobel;
+      asdfx[IMG_WIDTH*(i) + j] = sobel;
     }
   }
+	*/
 
   // Combine the two convolutions into the output image
   for (int i=1; i<ROWS-1; i++) {
