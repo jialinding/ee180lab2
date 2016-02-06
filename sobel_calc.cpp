73adb8e287bf0fb75610f33e92ed0379e28b8ae8
Jialin Ding <djialin09@gmail.com>
2016-02-05 23:06:24 -0800

    jialin comes again

--- sobel_calc.cpp
+++ sobel_calc.cpp
@@ -90,12 +90,19 @@ void sobelCalc(Mat& img_gray, Mat& img_sobel_out, int side)
   // Calculate the x convolution
   for (int i=1; i<ROWS-1; i++) {
     for (int j=col_begin; j<col_end; j++) {
-      sobel = abs(img_gray.data[IMG_WIDTH*(i-1) + (j-1)] -
-		  img_gray.data[IMG_WIDTH*(i+1) + (j-1)] +
-		  2*img_gray.data[IMG_WIDTH*(i-1) + (j)] -
-		  2*img_gray.data[IMG_WIDTH*(i+1) + (j)] +
-		  img_gray.data[IMG_WIDTH*(i-1) + (j+1)] -
-		  img_gray.data[IMG_WIDTH*(i+1) + (j+1)]);
+    //   sobel = abs(img_gray.data[IMG_WIDTH*(i-1) + (j-1)] -
+		  // img_gray.data[IMG_WIDTH*(i+1) + (j-1)] +
+		  // 2*img_gray.data[IMG_WIDTH*(i-1) + (j)] -
+		  // 2*img_gray.data[IMG_WIDTH*(i+1) + (j)] +
+		  // img_gray.data[IMG_WIDTH*(i-1) + (j+1)] -
+		  // img_gray.data[IMG_WIDTH*(i+1) + (j+1)]);
+
+      sobel = abs(img_gray.data[IMG_WIDTH*(i-1) + (j-1)] +
+      2*img_gray.data[IMG_WIDTH*(i-1) + (j)] +
+      img_gray.data[IMG_WIDTH*(i-1) + (j+1)] -
+      img_gray.data[IMG_WIDTH*(i+1) + (j-1)] -
+      2*img_gray.data[IMG_WIDTH*(i+1) + (j)] -
+      img_gray.data[IMG_WIDTH*(i+1) + (j+1)]);
 
       sobel = (sobel > 255) ? 255 : sobel;
       asdfx[IMG_WIDTH*(i) + (j)] = sobel;
