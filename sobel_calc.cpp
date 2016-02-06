c8b3813f787b83f7156c9b26d202b50f0c210574
napdivad <david.l.pan@gmail.com>
2016-02-05 23:39:20 -0800

    asdfasdf

--- sobel_calc.cpp
+++ sobel_calc.cpp
@@ -87,29 +87,21 @@ void sobelCalc(Mat& img_gray, Mat& img_sobel_out, int side)
     col_end = img_gray.cols-1;
   }
 
-  // Calc the y convolution
   for (int i=1; i<ROWS-1; i++) {
-		
+		// Calculate the x convolution
     for (int j=col_begin; j<col_end; j++) {
-      sobel = abs(img_gray.data[IMG_WIDTH*(i-1) + (j-1)] -
- 		   img_gray.data[IMG_WIDTH*(i-1) + (j+1)] +
- 		   2*img_gray.data[IMG_WIDTH*(i) + (j-1)] -
- 		   2*img_gray.data[IMG_WIDTH*(i) + (j+1)] +
- 		   img_gray.data[IMG_WIDTH*(i+1) + (j-1)] -
- 		   img_gray.data[IMG_WIDTH*(i+1) + (j+1)]);
-			/*
-      sobel = abs(img_gray.data[IMG_WIDTH*(i-1) + (j-1)] -
-			  img_gray.data[IMG_WIDTH*(i+1) + (j-1)] +
-			  2*img_gray.data[IMG_WIDTH*(i-1) + (j)] -
-			  2*img_gray.data[IMG_WIDTH*(i+1) + (j)] +
-			  img_gray.data[IMG_WIDTH*(i-1) + (j+1)] -
-			  img_gray.data[IMG_WIDTH*(i+1) + (j+1)]);
-			*/
+      sobel = abs(img_gray.data[IMG_WIDTH*(i-1) + (j-1)]
+				+ 2*img_gray.data[IMG_WIDTH*(i-1) + (j)]
+					+ img_gray.data[IMG_WIDTH*(i-1) + (j+1)]
+			  - img_gray.data[IMG_WIDTH*(i+1) + (j-1)]
+			  - 2*img_gray.data[IMG_WIDTH*(i+1) + (j)]
+			  - img_gray.data[IMG_WIDTH*(i+1) + (j+1)]);
 
       sobel = (sobel > 255) ? 255 : sobel;
       asdfx[IMG_WIDTH*(i) + j] = sobel;
     }
 		
+		// Calc the y convolution
     for (int j=col_begin; j<col_end; j++) {
      sobel = abs(img_gray.data[IMG_WIDTH*(i-1) + (j-1)] -
 		   img_gray.data[IMG_WIDTH*(i-1) + (j+1)] +
@@ -124,7 +116,6 @@ void sobelCalc(Mat& img_gray, Mat& img_sobel_out, int side)
     }
   }
 	
-  // Calculate the x convolution
 	/*
   for (int i=1; i<ROWS-1; i++) {
     for (int j=col_begin; j<col_end; j++) {
