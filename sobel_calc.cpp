ac697bd4cd3fa3fef07586e0a94e5e314a3c3602
Jialin Ding <djialin09@gmail.com>
2016-02-05 18:24:46 -0800

    try

--- sobel_calc.cpp
+++ sobel_calc.cpp
@@ -272,14 +272,14 @@ void sobelCalc(Mat& img_gray, Mat& img_sobel_out, int side)
   // Combine the two convolutions into the output image
   for (int i=1; i<img_gray.rows-1; i++) {
     for (int j=col_begin; j<col_end; j+=16) {
-      if (j + 16 >= col_end) {
-        for (j = j; j < col_end; j++) {
-          sobel = img_outx.data[IMG_WIDTH*(i) + j] +
-          img_outy.data[IMG_WIDTH*(i) + j];
-          sobel = (sobel > 255) ? 255 : sobel;
-          img_sobel_out.data[IMG_WIDTH*(i) + j] = sobel;
-        }
-      } else {
+      // if (j + 16 >= col_end) {
+      //   for (j = j; j < col_end; j++) {
+      //     sobel = img_outx.data[IMG_WIDTH*(i) + j] +
+      //     img_outy.data[IMG_WIDTH*(i) + j];
+      //     sobel = (sobel > 255) ? 255 : sobel;
+      //     img_sobel_out.data[IMG_WIDTH*(i) + j] = sobel;
+      //   }
+      // } else {
         sobel_data_x = vld1q_u8(&img_outx.data[IMG_WIDTH*(i) + j]);
         sobel_data_y = vld1q_u8(&img_outy.data[IMG_WIDTH*(i) + j]);
         sobel_data = vaddq_u8(sobel_data_x, sobel_data_y);
@@ -289,7 +289,7 @@ void sobelCalc(Mat& img_gray, Mat& img_sobel_out, int side)
         for (int k = 0; k < 16; k++) {
           img_sobel_out.data[IMG_WIDTH*(i) + j + k] = (sobel_out_final[k] > 255) ? 255 : sobel_out_final[k];
         }
-      }
+      // }
  //      sobel = img_outx.data[IMG_WIDTH*(i) + j] +
 	// img_outy.data[IMG_WIDTH*(i) + j];
  //      sobel = (sobel > 255) ? 255 : sobel;
